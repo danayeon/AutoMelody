8651dda26f3c50bd0877d5cd213a1c4f07858e5b
Noisy1014 <68069176+Noisy1014@users.noreply.github.com>
2020-07-17 11:26:15 +0900

    Update generate_melodyline.rb

--- generate_melodyline.rb
+++ generate_melodyline.rb
@@ -2,3 +2,8 @@
 baseline = ["A", "G#m(b5)", "A", "F#m"]
 # 各キーに対するコード
 c = ['C', 'E', 'G']
+csharp = ['C#', 'F', 'G#']
+dsharp = ['D#', 'G', 'A#']
+fsharp = ['F#', 'A#', 'G#']
+gsharp = ['G#', 'C', 'D#']
+asharp = ['A#', 'D', 'F']
