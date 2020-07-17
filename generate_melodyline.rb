dbf8c79b80e8b78cd8714742c4c2e948f9924772
danayeon <tigerwest1009@gmail.com>
2020-07-17 13:25:14 +0900

    Update generate_melodyline.rb

--- generate_melodyline.rb
+++ generate_melodyline.rb
@@ -44,53 +44,6 @@ class Key
   end
 end
 
-
-
-
-# 各キーに対するコード
-c = ['C', 'E', 'G']
-cb5 = ['C', 'E', 'Gb']
-csharp = ['C#', 'F', 'G#']
-csharpb5 = ['C#', 'E#', 'G']
-cm = ['C', 'Eb', 'G']
-csharpm = ['C#', 'E', 'G#']
-
-d = ['D', 'F#', 'A']
-db5 = ['D', 'F#', 'Ab']
-dsharp = ['D#', 'G', 'A#']
-dsharpb5 = ['D#', 'G', 'A']
-dm = ['D', 'F', 'A']
-dsharpm = ['D#', 'F#', 'A#']
-
-e = ['E', 'G#', 'B']
-eb5 = ['E', 'G#', 'A#']
-em = ['E', 'G', 'B']
-
-f = ['F', 'A', 'C']
-fb5 = ['F', 'A', 'B']
-fsharp = ['F#', 'A#', 'G#']
-fsharpb5 = ['F#', 'A#', 'C']
-fm = ['F', 'Ab', 'C']
-fsharpm = ['F#', 'A', 'C#']
-
-g = ['G', 'B', 'D']
-gb5 = ['G', 'B', 'Db']
-gsharp = ['G#', 'C', 'D#']
-gsharpb5 = ['G#', 'C', 'D']
-gm = ['G', 'Bb', 'D']
-gsharpm = ['G#', 'B', 'D#']
-
-a = ['A', 'C#', 'E']
-ab5 = ['A', 'C#', 'Eb']
-asharp = ['A#', 'D', 'F']
-asharpb5 = ['A#', 'D', 'E']
-am = ['A', 'C', 'E']
-asharpm = ['A#', 'Db', 'F']
-
-b = ['B', 'D#', 'F#']
-bb5 = ['B', 'D#', 'F']
-bm = ['B', 'D', 'F#']
-
 # 音符を決める 同時に音階も決める
 notes = [*1..16]
 hihi = Key.new(gets.chomp)
